d8e8f7dae8d78a0df1bd753de2a0ab6a2947a79f
Sampreeth-sv <sampreeth.sv@gmail.com>
2025-11-15 16:33:18 +0530

    Add files via upload

---
+++ frontend/src/utils/toast.js
@@ -0,0 +1,58 @@
+import { toast } from 'react-toastify';
+
+export const showSuccess = (message) => {
+  toast.success(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export const showError = (message) => {
+  toast.error(message, {
+    position: "top-right",
+    autoClose: 4000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export const showInfo = (message) => {
+  toast.info(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export const showWarning = (message) => {
+  toast.warning(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export const showLoading = (message) => {
+  return toast.loading(message);
+};
+
+export const updateLoading = (toastId, type, message) => {
+  toast.update(toastId, {
+    render: message,
+    type: type,
+    isLoading: false,
+    autoClose: 3000,
+  });
+};
